b82b08fbc60fae3bf514c574745fc2d058fca0af
dhruv-kanjariya0111 <dhruv.kanjariya@bosctechlabs.com>
2024-06-08 19:16:43 +0530

    Initial Commit

---
+++ helloWorld/helloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloWorldApp.swift
+//  helloWorld
+//
+//  Created by bosctech on 08/06/24.
+//
+
+import SwiftUI
+
+@main
+struct helloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
